22159d8bae61f8613973a030b0bb9a64f06f208f
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-05-07 19:37:47 +0300

    1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,28 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        return String.format("This animal is mostly %s." +
+                " It has %d paw%s and %s" +
+                " fur.", this.color, this.numberOfPaws, this.getNumberOfPaws(), this.getHasFur(hasFur));
+    }
+
+    private String getHasFur(boolean hasFur) {
+        return hasFur ? "a" : "no";
+    }
+
+    private String getNumberOfPaws() {
+        return numberOfPaws > 1 ? "s" : "";
+    }
+
 }
